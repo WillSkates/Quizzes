a042c6d5dda0f67c16ae713beebc93e70779ea0e
Will <will.skates@ntlworld.com>
2015-10-14 15:34:17 +0100

    Stupidly didn't realise that phpunit doesnt add the environment variables that you request before the bootstrap script is loaded in.

--- tests.bootstrap.php
+++ tests.bootstrap.php
@@ -3,12 +3,12 @@ require __DIR__ . '/vendor/autoload.php';
 
 //create the mysql database to use.
 $conn = new PDO(
-	'mysql:host=' . $_ENV['db_host'] . ';',
-	$_ENV['db_username'],
-	$_ENV['db_password'],
+	'mysql:host=localhost;',
+	'travis',
+	'',
 	[
     	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 	]
 );
 
-$conn->exec('CREATE DATABASE ' . $_ENV['db_name'] . ';');
+$conn->exec('CREATE DATABASE quizzes_tests;');
