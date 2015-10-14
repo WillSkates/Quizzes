8c7ddad0d4a82f01552598be2dba35b30562b3ac
Will <will.skates@ntlworld.com>
2015-10-14 15:41:34 +0100

    Adding a mysql connection and tests to check that we can connect and that the xml configuration is right.

---
+++ src/Storage/Mysql.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * This file is part of Quizzes
+ *
+ * (c) William Skates <will@stuffby.ws>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Quizzes\Storage;
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+class Mysql
+{
+
+	public function __construct($host, $username, $password, $dbname)
+	{
+
+		$paths = [
+			__DIR__ . '/ORM/entities.xml'
+		];
+
+		$dbParams = array(
+		    'driver'   => 'pdo_mysql',
+		    'host'     => $host,
+		    'user'     => $username,
+		    'password' => $password,
+		    'dbname'   => $dbname
+		);
+
+		$isDevMode = false;
+
+		$config = Setup::createXMLMetadataConfiguration($paths, $isDevMode);
+		$entityManager = EntityManager::create($dbParams, $config);
+
+	}
+
+}
---
+++ tests/Storage/MysqlTests.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This file is part of Quizzes
+ *
+ * (c) William Skates <will@stuffby.ws>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Quizzes\Tests;
+
+use Quizzes\Storage\Mysql;
+
+use PHPUnit_Framework_TestCase;
+
+class MySQLTests extends PHPUnit_Framework_TestCase
+{
+
+	public function testCanCreateAndConnect()
+	{
+		$mysql = new Mysql(
+			$_ENV['db_host'],
+			$_ENV['db_username'],
+			$_ENV['db_password'],
+			$_ENV['db_name']
+		);
+	}
+}
