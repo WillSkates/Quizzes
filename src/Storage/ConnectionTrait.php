1d7ba694a26d08767f00b6d9868881b88d9331be
Will <will.skates@ntlworld.com>
2015-10-14 16:02:30 +0100

    Moving Work into a connection trait.

---
+++ src/Storage/ConnectionTrait.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * This file is part of Quizzes
+ *
+ * (c) William Skates <will@stuffby.ws>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Quizzes\Storage;
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+class ConnectionTrait
+{
+
+    protected $entityManager;
+
+    /**
+     * Establish a connection to the database using the following:
+     * @param  string $driver   The database driver name e.g.pdo_mysql
+     * @param  string $host     
+     * @param  string $username
+     * @param  string $password 
+     * @param  string $dbname   
+     * @return self
+     */
+    public function establishConnection($driver, $host, $username, $password, $dbname)
+    {
+
+        $paths = [
+            __DIR__ . '/ORM/entities.xml'
+        ];
+
+        $dbParams = array(
+            'driver'   => $driver,
+            'host'     => $host,
+            'user'     => $username,
+            'password' => $password,
+            'dbname'   => $dbname
+        );
+
+        $isDevMode = false;
+
+        $config = Setup::createXMLMetadataConfiguration($paths, $isDevMode);
+        $this->entityManager = EntityManager::create($dbParams, $config);
+
+        return $this;
+
+    }
+}
--- src/Storage/Mysql.php
+++ src/Storage/Mysql.php
@@ -9,31 +9,21 @@
  */
 namespace Quizzes\Storage;
 
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
-
 class Mysql
 {
+
+    use ConnectionTrait;
 
     public function __construct($host, $username, $password, $dbname)
     {
 
-        $paths = [
-            __DIR__ . '/ORM/entities.xml'
-        ];
-
-        $dbParams = array(
-            'driver'   => 'pdo_mysql',
-            'host'     => $host,
-            'user'     => $username,
-            'password' => $password,
-            'dbname'   => $dbname
+        $this->establishConnection(
+            'pdo_mysql',
+            $host,
+            $username,
+            $password,
+            $dbname
         );
-
-        $isDevMode = false;
-
-        $config = Setup::createXMLMetadataConfiguration($paths, $isDevMode);
-        $entityManager = EntityManager::create($dbParams, $config);
 
     }
 }
