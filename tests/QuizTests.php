a6b66c3a2c89ebde450e46c1b36c34a3c4d73725
Will <will.skates@ntlworld.com>
2015-10-14 09:01:14 +0100

    Adding first test.

---
+++ tests/QuizTests.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Quizzes\Tests;
+
+use Quizzes\Quiz;
+
+class QuizTests extends PHPUnit_Framework_TestCase
+{
+
+	public function testCanCreate()
+	{
+		$quiz = new Quiz();
+	}
+
+}
