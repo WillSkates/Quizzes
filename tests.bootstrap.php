f2356fa3e8a5cc86ede72e3a1c29e17fd3e6fa9b
Will <will.skates@ntlworld.com>
2015-10-14 15:30:36 +0100

    Attempting to fix usage of the wrong environment variable names.

--- tests.bootstrap.php
+++ tests.bootstrap.php
@@ -4,8 +4,8 @@ require __DIR__ . '/vendor/autoload.php';
 //create the mysql database to use.
 $conn = new PDO(
 	'mysql:host=' . $_ENV['db_host'] . ';',
-	$_ENV['db_user'],
-	$_ENV['db_pass'],
+	$_ENV['db_username'],
+	$_ENV['db_password'],
 	[
     	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 	]
